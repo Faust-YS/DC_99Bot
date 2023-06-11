c3b4b428bb8329508b2cd4e618fea589a498952d
Faust-YS <63462125+Faust-YS@users.noreply.github.com>
2023-06-12 00:18:12 +0800

    Delete utils directory

--- utils/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    botToken: 'MTExNzQ2NjEyMTE3MjQxODY5MQ.Gpa6fn.dXlepNKiLuCmLVNywWvmhk1BT99NMhrhphHEwA',
-    clientId: '1117466121172418691',
-    guildId: '969585213892067379'
-}
